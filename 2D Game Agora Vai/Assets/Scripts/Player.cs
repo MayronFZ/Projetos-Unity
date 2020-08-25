c610d06cc60a9fb943feea0df32b8113a2677ddd
Mayron Martins <mayronmartinscosta@hotmail.com>
2020-08-24 23:52:01 -0300

    envio projetos unity

---
+++ 2D Game Agora Vai/Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    public int lives;
+    public int coins;
+    
+    public float forcaPulo;
+    public float velocidadeMaxima;
+
+    public Text TextLives;
+    public Text TextCoins;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextLives.text = lives.ToString();
+        TextCoins.text = coins.ToString();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float movimento = Input.GetAxis("Horizontal");
+         GetComponent<Rigidbody2D>().velocity = new Vector2(movimento*velocidadeMaxima,GetComponent<Rigidbody2D>().velocity.y);
+        
+        if (movimento < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;    
+
+        }else if(movimento > 0)
+        {
+           GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if(movimento > 0 || movimento < 0){
+        GetComponent<Animator>().SetBool("Walking",true);
+        }else{
+                    GetComponent<Animator>().SetBool("Walking",false);
+
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Space)){
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0,forcaPulo));
+            
+        }
+        
+    }
+}
